00f0c34127dcf0791d61d4ab297cf30692737e7b
pablogvrcia <pablo2000zgz@gmail.com>
2022-05-10 00:00:07 +0200

    Update creation script

--- obstacle_detection/build.sh
+++ obstacle_detection/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-rm -r CMakeFile
-mkdir CMakefile
+[ -d output/pcd ] || mkdir -p output/pcd
+[ -d output/png ] || mkdir -p output/png
+[ -d CMakefile ] || mkdir CMakefile
 cd CMakefile
 source /opt/ros/noetic/setup.bash
 cmake ..
